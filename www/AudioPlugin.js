4673eaeb405ed486747b30f794d33323bc9f2ccd
Rostyslav Stepanyak <rostyslav.stepanyak@sethq.com>
2015-07-15 13:42:59 +0300

    add require statement for cordova.exec in plugin.js

--- www/AudioPlugin.js
+++ www/AudioPlugin.js
@@ -1,18 +1,16 @@
-cordova.define("com-datamart-wfpk.AudioPlugin", function(require, exports, module) { cordova.define("com-datamart-wfpk.AudioPlugin", function(require, exports, module) { var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
 var AudioPlugin = {
-	play: function (successCallback, errorCallback, url) {
-		exec(successCallback, errorCallback, 'AudioPlugin', 'play', [url]);
-	},
-	pause: function(successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'AudioPlugin', 'pause', []);
-	},
-	changeBitrate: function(successCallback, errorCallback, bitrate) {
-    	exec(successCallback, errorCallback, 'AudioPlugin', 'changeBitrate', [bitrate]);
+    play: function (successCallback, errorCallback, url) {
+        alert('Before exec');
+        exec(successCallback, errorCallback, 'AudioPlugin', 'play', [url]);
+    },
+    pause: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'AudioPlugin', 'pause', []);
+    },
+    changeBitrate: function(successCallback, errorCallback, bitrate) {
+        exec(successCallback, errorCallback, 'AudioPlugin', 'changeBitrate', [bitrate]);
     }
 };
 
 module.exports = AudioPlugin;
-});
-
-});
