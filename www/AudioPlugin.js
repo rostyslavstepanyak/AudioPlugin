789390b36386f6ad370188e10ad5b1a9831b2605
Rostyslav Stepanyak <rostyslav.stepanyak@sethq.com>
2015-07-15 13:37:02 +0300

    fix plugin js

--- www/AudioPlugin.js
+++ www/AudioPlugin.js
@@ -1,16 +1,18 @@
-cordova.define("com-datamart-wfpk.AudioPlugin", function(require, exports, module) { var exec = require('cordova/exec');
+cordova.define("com-datamart-wfpk.AudioPlugin", function(require, exports, module) { cordova.define("com-datamart-wfpk.AudioPlugin", function(require, exports, module) { var exec = require('cordova/exec');
 
 var AudioPlugin = {
 	play: function (successCallback, errorCallback, url) {
 		exec(successCallback, errorCallback, 'AudioPlugin', 'play', [url]);
 	},
-	pause: (successCallback, errorCallback) {
+	pause: function(successCallback, errorCallback) {
 		exec(successCallback, errorCallback, 'AudioPlugin', 'pause', []);
 	},
-	changeBitrate: (successCallback, errorCallback, bitrate) {
-    		exec(successCallback, errorCallback, 'AudioPlugin', 'changeBitrate', [bitrate]);
+	changeBitrate: function(successCallback, errorCallback, bitrate) {
+    	exec(successCallback, errorCallback, 'AudioPlugin', 'changeBitrate', [bitrate]);
     }
 };
 
 module.exports = AudioPlugin;
+});
+
 });
