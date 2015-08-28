2380d9b16d974f3c9a82c78a9cdd64aed55f553c
Rostyslav Stepanyak <rostyslav.stepanyak@sethq.com>
2015-08-28 16:52:05 +0300

    extend volume handling

--- src/android/AudioPlugin.java
+++ src/android/AudioPlugin.java
@@ -19,97 +19,107 @@ import android.content.Intent;
 import java.io.IOException;
 
 public class AudioPlugin extends CordovaPlugin {
-
-	private static final String LOG_TAG = "Audio Player";
-	private String action;
-	private MediaPlayer mp = null;
-
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-
-	}
-
-	@Override
-	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		Log.v(LOG_TAG, "Received: " + action);
-		this.action = action;
-		final Activity activity = this.cordova.getActivity();
-		final Context context = activity.getApplicationContext();
-		cordova.setActivityResultCallback(this);
-		if(action.equals("create")) {
-			String streamUrl = args.getString(0);
-			create(activity, callbackContext, streamUrl);
-			return true;
-		}
-		if (action.equals("play")) {
-			play(activity, callbackContext);
-			return true;
-		}
-		if (action.equals("pause")) {
-			pause(activity, callbackContext);
-			return true;
-		}
-
-		if (action.equals("stop")) {
-			stop(activity, callbackContext);
-			return true;
-		}
+    
+    private static final String LOG_TAG = "Audio Player";
+    private String action;
+    private MediaPlayer mp = null;
+    
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        
+    }
+    
+    @Override
+    public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        Log.v(LOG_TAG, "Received: " + action);
+        this.action = action;
+        final Activity activity = this.cordova.getActivity();
+        final Context context = activity.getApplicationContext();
+        cordova.setActivityResultCallback(this);
+        if(action.equals("create")) {
+            String streamUrl = args.getString(0);
+            create(activity, callbackContext, streamUrl);
+            return true;
+        }
+        if (action.equals("play")) {
+            play(activity, callbackContext);
+            return true;
+        }
+        if (action.equals("pause")) {
+            pause(activity, callbackContext);
+            return true;
+        }
+        
+        if (action.equals("stop")) {
+            stop(activity, callbackContext);
+            return true;
+        }
         
         if (action.equals("isPlaying")) {
             isPlaying(activity, callbackContext);
             return true;
         }
-		return false;
-	}
-
-	private void create(final Activity activity, final CallbackContext callbackContext, final String url) {
-		cordova.getThreadPool().execute(new Runnable() {
-			@Override
-			public void run() {
-				//Just in case
-				if(mp != null && mp.isPlaying()) {
-					mp.stop();
-					mp = null;
-				}
-				if (mp == null) {
-					mp = new MediaPlayer();
-				}
-				try {
-					mp.reset();
-					mp.setDataSource(url);
-					mp.prepare();
+        
+        if (action.equals("getVolume")) {
+            getVolume(activity, callbackContext);
+            return true;
+        }
+        
+        if (action.equals("setVolume")) {
+            double volume = args.getDouble(0);
+            setVolume(activity, callbackContext, (float)volume);
+            return true;
+        }
+        return false;
+    }
+    
+    private void create(final Activity activity, final CallbackContext callbackContext, final String url) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                //Just in case
+                if(mp != null && mp.isPlaying()) {
+                    mp.stop();
+                    mp = null;
+                }
+                if (mp == null) {
+                    mp = new MediaPlayer();
+                }
+                try {
+                    mp.setDataSource(url);
+                    mp.prepare();
                     callbackContext.success(handleResult());
-				}
-				catch (IOException e) {
-					callbackContext.error(e.getLocalizedMessage());
-				}
-			}
-		});
-	}
-
-	private void play(final Activity activity, final CallbackContext callbackContext) {
-		cordova.getThreadPool().execute(new Runnable() {
-			@Override
-			public void run() {
-				 if(mp != null) {
-					 if(!mp.isPlaying()) {
-						 mp.start();
-					 }
-				 }
-                 callbackContext.success(handleResult());
-			}
-		});
-	}
-
-	private void pause(final Activity activity, final CallbackContext callbackContext) {
-		cordova.getThreadPool().execute(new Runnable() {
-			@Override
-			public void run() {
-				mp.pause();
-				callbackContext.success(handleResult());
-			}
-		});
-	}
+                }
+                catch (IOException e) {
+                    callbackContext.error(e.getLocalizedMessage());
+                }
+            }
+        });
+    }
+    
+    private void play(final Activity activity, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                if(mp != null) {
+                    if(!mp.isPlaying()) {
+                        mp.start();
+                    }
+                }
+                callbackContext.success(handleResult());
+            }
+        });
+    }
+    
+    private void pause(final Activity activity, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                mp.pause();
+                callbackContext.success(handleResult());
+            }
+        });
+    }
     
     private void isPlaying(final Activity activity, final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
@@ -119,19 +129,44 @@ public class AudioPlugin extends CordovaPlugin {
             }
         });
     }
-
-	private void stop(final Activity activity, final CallbackContext callbackContext) {
-		cordova.getThreadPool().execute(new Runnable() {
-			@Override
-			public void run() {
-				if(mp != null) {
-					mp.stop();
-					mp = null;
-				}
-				callbackContext.success(handleResult());
-			}
-		});
-	}
+    
+    private void stop(final Activity activity, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                if(mp != null) {
+                    mp.stop();
+                    mp = null;
+                }
+                callbackContext.success(handleResult());
+            }
+        });
+    }
+    
+    private void setVolume(final Activity activity, final CallbackContext callbackContext, final float volume) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                if(mp != null) {
+                    callbackContext.error("Player not initialised");
+                }
+                
+                callbackContext.success(handleSetVolumeResult(activity, volume));
+            }
+        });
+    }
+    
+    private void getVolume(final Activity activity, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                if(mp != null) {
+                    callbackContext.error("Player not initialised");
+                }
+                callbackContext.success(handleVolumeResult(activity));
+            }
+        });
+    }
     
     private JSONObject isPlayingInJSON() {
         JSONObject response = new JSONObject();
@@ -148,14 +183,51 @@ public class AudioPlugin extends CordovaPlugin {
         }
         return response;
     }
-
-	private JSONObject handleResult() {
-		JSONObject response = new JSONObject();
-		try {
-			response.put("status", "OK");
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return response;
-	}
+    
+    private JSONObject handleResult() {
+        JSONObject response = new JSONObject();
+        try {
+            response.put("status", "OK");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+    
+    private JSONObject handleSetVolumeResult(Activity activity, float volume) {
+        JSONObject response = new JSONObject();
+        try {
+            AudioManager audioManager = (AudioManager) activity.getSystemService(Context.AUDIO_SERVICE);
+            int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            int newVolume = (int) (maxVolume*volume);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVolume, 0);
+            response.put("status", "OK");
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+    
+    private JSONObject handleVolumeResult(Activity activity) {
+        JSONObject response = new JSONObject();
+        try {
+            AudioManager audioManager = (AudioManager)  activity.getSystemService(Context.AUDIO_SERVICE);
+            int origionalVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            int volume_level= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            response.put("volume", volume_level*1.0 / origionalVolume);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return response;
+    }
 }
